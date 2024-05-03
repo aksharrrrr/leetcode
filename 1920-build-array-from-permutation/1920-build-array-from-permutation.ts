8ec6c0a5e128afc3d771190d2f90044b46614776
Akshar Rughani <51110091+aksharrrrr@users.noreply.github.com>
2024-05-03 08:26:51 +0530

    Time: 59 ms (95.07%), Space: 54 MB (89.16%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.ts
@@ -0,0 +1,8 @@
+function buildArray(nums: number[]): number[] {
+    let ans: number[] = Array(nums.length);
+    
+    for(let i =0; i<nums.length; i++) 
+        ans[i] = nums[nums[i]];
+    
+    return ans;
+};
