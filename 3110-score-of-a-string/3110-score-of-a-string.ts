42b438fd0f660d45f9046834b0664caec8facce8
Akshar Rughani <51110091+aksharrrrr@users.noreply.github.com>
2024-05-07 07:44:49 +0530

    Time: 70 ms (33.58%), Space: 52.1 MB (62.56%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.ts
+++ 3110-score-of-a-string/3110-score-of-a-string.ts
@@ -1,7 +1,9 @@
 function scoreOfString(s: string): number {
-    let sum: number = 0;
-    for(let i=0; i<s.length-1; i++){
-        sum += Math.abs(s.charCodeAt(i)-s.charCodeAt(i+1));
+    let result = 0;
+
+    for(let i = 0; i < s.length - 1 ; i++){
+        result += Math.abs(s[i].charCodeAt(0) - s[i + 1].charCodeAt(0))
     }
-    return sum;
+
+    return result
 };
