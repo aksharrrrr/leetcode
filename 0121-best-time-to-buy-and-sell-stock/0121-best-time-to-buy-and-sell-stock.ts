f6824a25b865d04e9b8750ae7c2356012838c945
Akshar Rughani <51110091+aksharrrrr@users.noreply.github.com>
2024-05-21 07:45:45 +0530

    Time: 84 ms (35.28%), Space: 59.1 MB (97.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,13 +1,9 @@
 function maxProfit(prices: number[]): number {
     let profit: number = 0;
-    for(let i=0; i<prices.length-1; i++){
-        for(let j=i+1; j<prices.length; j++){
-            if(prices[j]-prices[i] > 0 && prices[j]-prices[i] >= profit){
-                profit = prices[j]-prices[i];
-            }else if(prices[j]-prices[i] <= 0){
-                break;
-            }
-        }
+    let minPrice: number = Number.MAX_SAFE_INTEGER;
+    for(let i=0; i<prices.length; i++){
+       minPrice = Math.min(minPrice, prices[i]);
+        profit = Math.max(profit, prices[i]-minPrice);
     }
     return profit;
 };
