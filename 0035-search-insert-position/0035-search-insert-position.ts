a973fd472a0b0c2f8412c145715292ea7e44d04f
Akshar Rughani <51110091+aksharrrrr@users.noreply.github.com>
2024-11-11 22:16:28 +0530

    Time: 0 ms (100.00%), Space: 51.2 MB (82.92%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.ts
@@ -0,0 +1,16 @@
+function binarySearch(nums: number[], low: number, high: number, target: number): number {
+    if(low > high) return high+1;
+    let mid: number = Math.floor((low+high)/2);
+    if(nums[mid] == target) 
+        return mid;
+    if(nums[mid] > target) 
+        return binarySearch(nums, low, mid-1, target);
+    else
+        return binarySearch(nums, mid+1, high, target);
+}
+
+function searchInsert(nums: number[], target: number): number {
+    let low: number = 0;
+    let high: number = nums.length-1;
+    return binarySearch(nums, low, high, target);
+};
